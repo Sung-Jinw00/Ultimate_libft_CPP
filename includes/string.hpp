c3469445f45f1f215840eba5e9f747086292378d
Louis Cagnion <locagnio@c3r1p3.42perpignan.fr>
2025-11-28 19:33:11 +0100

    multi stripe added

--- includes/string.hpp
+++ includes/string.hpp
@@ -5,3 +5,5 @@
 enum class StripSide { LEFT, RIGHT, BOTH };
 
 void	stripe(std::string &s, StripSide side = StripSide::BOTH);
+void	stripe(std::string &s, char c, StripSide side = StripSide::BOTH);
+void	stripe(std::string &s, const std::string &set, StripSide side = StripSide::BOTH);
